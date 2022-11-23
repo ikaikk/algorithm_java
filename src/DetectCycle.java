2fddb0574875ce2e89d5b061aebee8e896978523
ikaikk <ikakk@hotmail.com>
2022-11-24 02:24:51 +0800

    相交列表

---
+++ src/DetectCycle.java
@@ -0,0 +1,41 @@
+/**
+ * 力扣142，环形列表2，返回链表开始入环的第一个节点
+ */
+public class DetectCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (null == head) {
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+        boolean isCycle = false;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        if (isCycle) {
+            ListNode ptr = head;
+            while (ptr != slow) {
+                ptr = ptr.next;
+                slow = slow.next;
+            }
+
+            return ptr;
+        }
+
+        return null;
+    }
+}
---
+++ src/IntersectionNode.java
@@ -0,0 +1,34 @@
+/**
+ * 力扣160，相交链表
+ */
+public class IntersectionNode {
+    static class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (null == headA || null == headB) {
+            return null;
+        }
+
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+
+        while (ptrA != ptrB) {
+            if (ptrA == null) {
+                ptrA = headB;
+            } else {
+                ptrA = ptrA.next;
+            }
+
+            if (ptrB == null) {
+                ptrB = headA;
+            } else {
+                ptrB = ptrB.next;
+            }
+        }
+
+        return ptrA;
+    }
+}
