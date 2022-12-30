1b07db0a6d2da48dc50152ea230455f72d77b8ca
ikaikk <ikakk@hotmail.com>
2022-12-30 14:25:49 +0800

    选择排序

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -1,28 +1,30 @@
 package sort;
 
 public class SelectSort {
-    public void selectSort(int[] arr) {
-        int len = arr.length;
-        for (int i = 0; i < len - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < len; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
-            }
 
-            if (min != i) {
-                int temp = arr[i];
-                arr[i] = arr[min];
-                arr[min] = temp;
+    public static void selectSort(int[] arr) {
+        if (null == arr || arr.length == 1) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j <arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    change(arr, i, j);
+                }
             }
         }
     }
 
+    private static void change(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 6, 2, 3, 10};
-        SelectSort selectSort = new SelectSort();
-        selectSort.selectSort(arr);
+        selectSort(arr);
         for (int i : arr) {
             System.out.print(i);
             System.out.print(",");
